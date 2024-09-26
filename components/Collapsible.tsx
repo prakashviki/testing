56603c335f69e409ae275dd4eda5671874a65685
prakashviki <prakashpalani147@gmail.com>
2024-09-26 05:52:56 +0530

    Description of changes

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -13,7 +13,7 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
   return (
     <ThemedView>
       <TouchableOpacity
-        style={styles.heading}
+        style={ styles.heading}
         onPress={() => setIsOpen((value) => !value)}
         activeOpacity={0.8}>
         <Ionicons
